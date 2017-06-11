b9bf81ea42321f690039b3ee65471a0d1d2ed0f7
Scott McCutchen <scott.mccutchen@soverance.com>
2017-06-11 19:35:01 -0400

    Updated LAMP template

--- Azure/Templates/LAMP_Deploy/install_lamp.sh
+++ Azure/Templates/LAMP_Deploy/install_lamp.sh
@@ -15,7 +15,7 @@ sudo apt-get -y install mysql-server
 # install PHP
 # We're deploying Unbuntu 17.04 with this script, which has updated to PHP 7.0 by default.
 # If we must continue using PHP 5, we'll need to deploy Unbuntu 14.04 LTS
-# libapache2-mod-php will install a virtual package that depends on the latest version of PHP, pulling libapache2-mod-php7.0 as a dependency
+# libapache2-mod-php will install a virtual package that depends on the latest version of PHP, and will automatically pull common dependent modules
 # This command also installs the php-mysql module - other modules can be added to enhance it's functionality. Find them with command:  "apt-cache search php- | less"
 sudo apt-get -y install php libapache2-mod-php php-mysql 
 
