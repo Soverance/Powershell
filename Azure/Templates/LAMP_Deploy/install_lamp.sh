1ac5cace0a0cc88a0c6bd20ba22dcd5b55cb82de
Scott McCutchen <scott.mccutchen@soverance.com>
2017-06-11 19:22:57 -0400

    Another update

--- Azure/Templates/LAMP_Deploy/install_lamp.sh
+++ Azure/Templates/LAMP_Deploy/install_lamp.sh
@@ -8,14 +8,19 @@ export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.6 mysql-server/root_password password $dbpass | sudo debconf-set-selections
 echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | sudo debconf-set-selections
 
-# install the LAMP stack
-sudo apt-get -y install apache2 
-sudo apt-get -y install mysql-server 
-sudo apt-get -y install php5 
-sudo apt-get -y install php5-mysql  
+# install Apache 2
+sudo apt-get -y install apache2
+# install MySQL Server
+sudo apt-get -y install mysql-server
+# install PHP
+# We're deploying Unbuntu 17.04 with this script, which has updated to PHP 7.0 by default.
+# If we must continue using PHP 5, we'll need to deploy Unbuntu 14.04 LTS
+# libapache2-mod-php will install a virtual package that depends on the latest version of PHP, pulling libapache2-mod-php7.0 as a dependency
+# This command also installs the php-mysql module - other modules can be added to enhance it's functionality. Find them with command:  "apt-cache search php- | less"
+sudo apt-get -y install php libapache2-mod-php php-mysql 
 
 # write some PHP
-#sudo echo '<center><h1>BKV Client Linux Deployment Test Page</h1></center\>' > /var/www/html/phpinfo.php
+sudo echo '<center><h1>BKV Client Linux Deployment Test Page</h1></center\>' > /var/www/html/phpinfo.php
 sudo echo '<?php phpinfo(); ?>' >> /var/www/html/phpinfo.php
 
 # restart Apache
