0112f978a838d3bc254de7c64966279a5eee6b99
Scott McCutchen <smccutchen@users.noreply.github.com>
2017-12-16 20:54:02 -0500

    Remove Product Key

---
+++ Windows/Remove-ProductKey.ps1
@@ -0,0 +1,16 @@
+# scott.mccutchen@soverance.com
+
+# Remove the product key from a Windows installation, and prepare it for a new user.
+# Use this only if you want to completely remove the activation key!
+
+# uninstall the current product key and move Windows into "Unlicensed Mode"
+slmgr /upk
+
+# remove the product key from the registry, so that it cannot be stolen later
+slmgr /cpky
+
+# reset the Windows Activation timers so that the new owners will be prompted to activate Windows.
+slmgr /rearm
+
+# you must reboot the computer for these changes to take effect.
+Restart-Computer
