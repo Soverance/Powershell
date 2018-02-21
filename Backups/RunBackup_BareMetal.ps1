5e642138c9b8ca4081af0d361545b35bd39f7c0e
Scott McCutchen <scott.mccutchen@soverance.com>
2018-02-21 14:30:48 -0500

    backup updates

---
+++ Backups/RunBackup_BareMetal.ps1
@@ -0,0 +1,8 @@
+# © 2018 Soverance Studios
+# Scott McCutchen
+# soverance.com
+# scott.mccutchen@soverance.com
+
+cd "C:\Windows\System32\"
+
+./wbadmin start backup -backupTarget:"\\sov-data\backups" -allcritical -quiet
