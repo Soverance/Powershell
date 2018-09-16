8ad7742a60ccc84c9556556df605570d249e1216
Scott McCutchen <smccutchen@users.noreply.github.com>
2018-09-15 21:24:45 -0400

    updated soverance db deploy

--- Backups/soverance.com.db.deployment.sql
+++ Backups/soverance.com.db.deployment.sql
@@ -14,19 +14,28 @@ GO
 CREATE TABLE [Post] (
     [PostId] int NOT NULL IDENTITY,
     [CategoryId] int NOT NULL,    
-    [Date] nvarchar(max),
-    [Image] nvarchar(max),
+    [PostType] int NOT NULL, 
+    [Slug] nvarchar(max),
+    [Date] nvarchar(max),    
     [Title] nvarchar(max),
+    [Description] nvarchar(max),
 	[Content] nvarchar(max),
     [Author] nvarchar(max),
+    [VideoUrl] nvarchar(max),
+    [Slider1] nvarchar(max),
+    [Slider2] nvarchar(max),
+    [Slider3] nvarchar(max),
+    [PlaylistId] nvarchar(max),
     CONSTRAINT [PK_Post] PRIMARY KEY ([PostId]),
     CONSTRAINT [FK_Post_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([CategoryId]) ON DELETE CASCADE
 );
 GO
 
 INSERT INTO [Category] (CategoryName) VALUES
-('Soverance Blog'),
+('General'),
+('Portfolio'),
 ('Tutorials'),
+('Motorcycles'),
 ('Ethereal Legends'),
 ('Endless Reach')
 GO
